fd2b5be126505216fac7aed220418b0c4b4b494b
haigang-shushu <haigangshushu@gmail.com>
2022-11-11 22:18:29 +0800

    3. 变量与常量

---
+++ src/variableconstand.java
@@ -0,0 +1,13 @@
+public class variableconstand {
+//    public static void main(String[] args) {
+//        int x = 10;
+//        int y = 20;
+//        System.out.println(x);
+//        System.out.println(y);
+//    };
+    public static void main(String[] args) {
+        final int a = 666; // 在变量前添加 final 关键字，表示这是一个常量
+        a = 777; // 常量的值不允许发生修改
+    }
+}
+
