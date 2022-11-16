f5604a36492844f1715e3567da9e420fad0a5d27
haigang-shushu <haigangshushu@gmail.com>
2022-11-16 20:53:10 +0800

    6. char 字符类型

--- src/Char.java
+++ src/Char.java
@@ -1,6 +1,17 @@
-public class Char {
-    public static void main(String[] args){
-        char c = 'A';
-        System.out.println(c);
+//public class Char {
+//    public static void main(String[] args){
+//        char c = 'A';
+//        // 转换成 int 类型就是阿斯克码
+//        int a = c;
+//        System.out.println(a);
+//    }
+//}
+
+// Java 程序在编译 .class 文件之后， 会采用 utf - 8 的编码格式，支持的字符也非常多，
+
+public class Char{
+    public static void main(String[] args) {
+        // 字符串需要使用双引号囊括，字符串中可以包含 0 - N 个字符
+        String str = "啊啊啊";
     }
 }
