cbfd1852a21e62a26dd18835a3e756d6cd288ad1
haigang-shushu <haigangshushu@gmail.com>
2022-11-16 21:03:47 +0800

    7. Boolean 字符类型

---
+++ src/Boolean.java
@@ -0,0 +1,9 @@
+// 布尔类型只有 true 和 false 两种值， 要么真，或者假 布尔类型 的变量
+// 通常 作 流程控制判断语句， 布尔类型占据空间大小并未明确定义，而是根据不同的 JVM 会有不同的实现
+
+public class Boolean {
+    public static void main(String[] args) {
+        boolean b = true;  // 只能是 true 和 false
+        System.out.println(b);
+    }
+}
