5fd6e3a003a974767fde64af8fa1daa854cf7b8e
haigang-shushu <haigangshushu@gmail.com>
2022-11-26 22:05:10 +0800

    9. 算术运算符

--- src/arithmeticOperator18.java
+++ src/arithmeticOperator18.java
@@ -18,13 +18,31 @@ public class arithmeticOperator18 {
         // 得到 - 10
 //        System.out.println(a);
 
-        String str1 = "Hello";
-        String str2 = "world";
+//        String str1 = "Hello";
+//        String str2 = "world";
 
 // 拼接 字符串
-        String str3 = str1 + str2;
+//        String str3 = str1 + str2;
 
-        System.out.println(str3);
+//        System.out.println(str3);
 
+
+        // 整数与小数一起计算时，所有的整数都会变成小数参与运算，所以最后结果也是小数
+//        double a = 8.0;
+//        int b = 5;
+//        System.out.println(a / b);  // 1.6
+
+        // 希望两个整数需要进行计算，但是想得到一个小数的结果怎么办
+//        int a = 8, b = 5;
+//        double c = a; // 可以将 其先隐式转换为小数类型，再转换后小数变量去参与计算/
+//        System.out.println( c / b );  // 同样可以得到正确的结果
+
+
+        // 取余数
+//      int a = 10;
+//        System.out.println(a % 3);
+
+        System.out.println(10 + 3 * 4);
     }
+
 }
