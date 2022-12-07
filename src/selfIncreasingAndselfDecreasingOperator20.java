ba6c3dc75f6be2b7785c8bf6edb5e9b597960c7a
haigang-shushu <haigangshushu@gmail.com>
2022-12-07 21:23:02 +0800

    11. 自增自减运算符

--- src/selfIncreasingAndselfDecreasingOperator20.java
+++ src/selfIncreasingAndselfDecreasingOperator20.java
@@ -1,4 +1,4 @@
-public class selfIncreasingAndselfDecreasingOperator19 {
+public class selfIncreasingAndselfDecreasingOperator20 {
     public static void main(String[] args) {
         int a = 8;
 //        a++;    // 自增运算符就是两个加号连在一起，效果跟上面一样的，a 都会自增 1
@@ -10,11 +10,16 @@ public class selfIncreasingAndselfDecreasingOperator19 {
 //        int b = ++a; // 先自增，再出结果 为 9
 //        System.out.println(b);
 //        System.out.println(a);
-        int b = -a++ + ++a;
+//        int b = -a++ + ++a;
         // 前面的 a 因为是正负号和自增是 同一个优先级，结合性是从右往左，所以说先计算 a++
         // a++ 的结果还是 8 然后是负号，得到 -8
         // 接着是后面的 a 因为此时 a 已经经过前面变成 9 所以说 ++a 就是先自增 再得到 10
         // 最后得到的结果 为 -8 + 10 = 2
-        System.out.println(b);
+//        System.out.println(b);
+        // a = a + 4
+        //a += 4;   // 等于 12
+        //int b = a += 4;  // += 的 运算结果就是自增之后的结果等于 12
+        a *= 9; // 跟 a = a * 9 等价阿 等于 72
+        System.out.println(a);
     }
 }
